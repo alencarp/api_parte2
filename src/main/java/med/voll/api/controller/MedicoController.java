90d8b289fd6330a76eedc3dbdc577c36297864c0
alencarp <patriciaalencar775@gmail.com>
2024-07-01 13:37:18 -0300

    Get medicos funcionando

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,12 +4,12 @@ import jakarta.validation.Valid;
 import med.voll.api.controller.medico.Medico;
 import med.voll.api.controller.medico.MedicoRepository;
 import med.voll.api.controller.medico.MedicoRequestDTO;
+import med.voll.api.controller.medico.MedicoResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +22,22 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid MedicoRequestDTO medicoRequestDTO) {
         medicoRepository.save(new Medico(medicoRequestDTO));
+    }
+
+    @GetMapping
+    public List<MedicoResponseDTO> listar() {
+        //map() mapeia de MedicoResponseDTO para Medico
+        return medicoRepository.findAll().stream().map(MedicoResponseDTO::new).toList();
+
+
+        /**
+         * Function<String, Integer> funcao = new Function<String, Integer>() {
+         *                        @Override
+         *            public Integer apply(String s) {
+         * 				return s.length();
+         *            }        * 		};
+         * 		Comparator<String> compardorDeStrings = Comparator.comparing(funcao);
+         * 		palavras.sort(compardorDeStrings);
+         */
     }
 }
