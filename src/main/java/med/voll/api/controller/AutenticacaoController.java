c9233444942f95f7299decbd387635fda3e6ed2f
alencarp <patriciaalencar775@gmail.com>
2024-07-02 16:51:21 -0300

    Adicionado Spring Security

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,36 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+
+    // preciso chamar o método AutenticacaoService.loadUserByUsername(String username) que faz a autenticação
+    //Porém, no Spring não chamamos direto a classe, mas chamamos a AutenticationManager, que por baixo dos panos vai chamar ela
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+
+        //converte do nosso dto para o UsernamePasswordAuthenticationToken(que é tipo um dto do Spring)
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+
+        //retorna um objeto que representa o usuario autenticado
+        Authentication authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+}
